9dd81458ffa1d8f3a8202881fa5f17ac957ac7a9
ErGovindthakur <ergovindthakur@gmail.com>
2025-05-07 00:38:57 +0530

    Explore full zustand with an example and also learnt how to persit zustand data in localstorage

--- zustand-docker/src/App.tsx
+++ zustand-docker/src/App.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useCountStore } from "./store/store";
+
+const zustandStoreTracker = ():number => {
+  const tracker = useCountStore.getState().count1;
+  return tracker;
+}
 
 const App: React.FC = () => {
+  const countDataZustand = useCountStore((countStore)=>countStore.count1);
+
+  const increment = useCountStore((state)=>state.increment);
+  const decrement = useCountStore((state)=>state.decrement);
+  const reset = useCountStore((state)=>state.reset);
+
+  useEffect(()=>{
+    console.log(zustandStoreTracker());
+  },[])
+
   return (
     <>
+    <h1>Counted Data is here -: {countDataZustand}</h1>
+    <button onClick={increment}>Increment</button>
+    <button onClick={decrement}>Decrement</button>
+    <button onClick={reset}>Reset</button>
       <OtherComponents userName={"Ajay"} />
     </>
   );
